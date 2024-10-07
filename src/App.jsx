260ccbf5b7e105c1f5837c5eb5e401661536ac7e
Mykola Bilenkiy <nikolosknv@gmail.com>
2024-10-07 15:14:28 +0300

    add reset

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,9 @@ export const goods = [
 
 export const App = () => {
   const [selectedGood, setSelectedGood] = useState('Jam');
+  const reset = () => {
+    setSelectedGood('');
+  };
 
   return (
     <main className="section container">
@@ -27,7 +30,7 @@ export const App = () => {
             data-cy="ClearButton"
             type="button"
             className="delete ml-3"
-            onClick={() => setSelectedGood('')}
+            onClick={reset}
           />
         </h1>
       ) : (
@@ -52,7 +55,7 @@ export const App = () => {
                     data-cy="RemoveButton"
                     type="button"
                     className="button is-info"
-                    onClick={() => setSelectedGood('')}
+                    onClick={reset}
                   >
                     -
                   </button>
